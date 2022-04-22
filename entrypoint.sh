2e04572292759570ae4749a807667825af9d53c0
Rishabh Kanojiya <rishabh.kanojiya@sportzinteractive.net>
2022-04-23 02:33:51 +0530

    copy brach

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,19 +31,11 @@ ${AWS_REGION}
 text
 EOF
 
-echo -e "\n-----Backing Up Files-------\n"
+echo -e "\n-----Copying Files-------\n"
 
-sh -c "aws s3 sync s3://${AWS_S3_BUCKET}/${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR:-./backup}/$(date +%Y-%m-%d:%H-%M) \
-              --exclude '*' --include 'js/*' --include 'asset-manifest.json' \
-              --include 'static/*' --include 'css/*' --include 'translations/*' \
-              --profile s3-copy-rm-profile \
+sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
+              --profile s3-sync-action \
               --no-progress"
-
-echo -e "\n-----Removing Files-------\n"
-
-sh -c "aws s3 rm s3://${AWS_S3_BUCKET}/${SOURCE_DIR:-.} --exclude '*' \
-              --include 'js/*' --include 'static/*' --recursive \
-              --profile s3-copy-rm-profile"
 
 aws configure --profile s3-copy-rm-profile <<-EOF >/dev/null 2>&1
 null
